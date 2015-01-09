baed57c1444f951eb63e8aa3e92acc063ace7af8
Scott Ware <scottdware@gmail.com>
2015-01-09 12:35:36 -0500

    Added functions, and documentation

--- junos.go
+++ junos.go
@@ -1,44 +1,88 @@
 package junos
 
 import (
+	"encoding/xml"
+	"errors"
 	"fmt"
 	"github.com/Juniper/go-netconf/netconf"
 	"log"
 )
 
+// Session holds the connection information to our Junos device.
 type Session struct {
 	Conn *netconf.Session
 }
 
+// RollbackXML parses our configuration after requesting it via rollback.
+type RollbackXML struct {
+	XMLName xml.Name `xml:"rollback-information"`
+	Config  string   `xml:"configuration-information>configuration-output"`
+}
+
+// NewSession establishes a new connection to a Junos device that we will use
+// to run our commands against.
 func NewSession(host, user, password string) *Session {
-    s, err := netconf.DialSSH(host, netconf.SSHConfigPassword(user, password))
-    if err != nil {
-        log.Fatal(err)
-    }
+	s, err := netconf.DialSSH(host, netconf.SSHConfigPassword(user, password))
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    return &Session{
-        Conn: s,
-    }
+	return &Session{
+		Conn: s,
+	}
 }
 
+// Lock locks the candidate configuration.
 func (s *Session) Lock() {
-	resp, err := s.Conn.Exec("<rpc><lock><target><candidate/></target></lock></rpc>")
-	if err != nil {
-		fmt.Printf("Error: %+v\n", err)
+	lockRPC := "<rpc><lock><target><candidate/></target></lock></rpc>"
+	resp, _ := s.Conn.Exec(lockRPC)
+	// if err != nil {
+	// log.Fatal(err)
+	// }
+
+	if resp.Ok == false {
+		for _, m := range resp.Errors {
+			fmt.Printf("%s\n", m.Message)
+		}
 	}
-    
-    fmt.Println(resp.RawReply)
 }
 
+// Unlock unlocks the candidate configuration.
 func (s *Session) Unlock() {
-	resp, err := s.Conn.Exec("<rpc><unlock><target><candidate/></target></unlock></rpc>")
-	if err != nil {
-		fmt.Printf("Error: %+v\n", err)
+	unlockRPC := "<rpc><unlock><target><candidate/></target></unlock></rpc>"
+	resp, _ := s.Conn.Exec(unlockRPC)
+	// if err != nil {
+	// fmt.Printf("Error: %+v\n", err)
+	// }
+
+	if resp.Ok == false {
+		for _, m := range resp.Errors {
+			fmt.Printf("%s\n", m.Message)
+		}
 	}
-    
-    fmt.Println(resp.RawReply)
 }
 
+// GetRollbackConfig returns the configuration of the given rollback state.
+func (s *Session) GetRollbackConfig(number int) (string, error) {
+	rpcCommand := fmt.Sprintf("<rpc><get-rollback-information><rollback>%d</rollback><format>text</format></get-rollback-information></rpc>", number)
+	reply, _ := s.Conn.Exec(rpcCommand)
+	rb := &RollbackXML{}
+
+	if reply.Ok == false {
+		for _, m := range reply.Errors {
+			return "", errors.New(m.Message)
+		}
+	}
+
+	err := xml.Unmarshal([]byte(reply.Data), rb)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return rb.Config, nil
+}
+
+// Close disconnects and closes the session to our Junos device.
 func (s *Session) Close() {
-    s.Conn.Close()
+	s.Conn.Close()
 }
