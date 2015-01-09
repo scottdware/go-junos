6b07ddff2a5f6d9220ae48df10fab8a3fc15610b
Scott Ware <scottdware@gmail.com>
2015-01-09 13:38:56 -0500

    Added additional RPC commands

--- rpc.go
+++ rpc.go
@@ -1,5 +1,9 @@
 package junos
 
 var RPCCommand = map[string]string{
-	"get-rescue-information": "<rpc><get-rescue-information><format>text</format></get-rescue-information></rpc>",
+	"lock":                             "<rpc><lock><target><candidate/></target></lock></rpc>",
+	"unlock":                           "<rpc><unlock><target><candidate/></target></unlock></rpc>",
+	"get-rescue-information":           "<rpc><get-rescue-information><format>text</format></get-rescue-information></rpc>",
+	"get-rollback-information":         "<rpc><get-rollback-information><rollback>%d</rollback><format>text</format></get-rollback-information></rpc>",
+	"get-rollback-information-compare": "<rpc><get-rollback-information><rollback>0</rollback><compare>%d</compare><format>text</format></get-rollback-information></rpc>",
 }
