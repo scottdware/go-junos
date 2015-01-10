394f46a3c0804747706a852cdfdc438f01a98a39
Scott Ware <scottdware@gmail.com>
2015-01-10 09:40:13 -0500

    Added additional RPC commands

--- rpc.go
+++ rpc.go
@@ -4,11 +4,12 @@ package junos
 var rpcCommand = map[string]string{
 	"command":                          "<rpc><command format=\"text\">%s</command></rpc>",
 	"command-xml":                      "<rpc><command format=\"xml\">%s</command></rpc>",
-    "commit": "<rpc><commit-configuration/></rpc>",
+	"commit":                           "<rpc><commit-configuration/></rpc>",
 	"get-rescue-information":           "<rpc><get-rescue-information><format>text</format></get-rescue-information></rpc>",
 	"get-rollback-information":         "<rpc><get-rollback-information><rollback>%d</rollback><format>text</format></get-rollback-information></rpc>",
 	"get-rollback-information-compare": "<rpc><get-rollback-information><rollback>0</rollback><compare>%d</compare><format>text</format></get-rollback-information></rpc>",
-	"lock":                             "<rpc><lock><target><candidate/></target></lock></rpc>",
+	"lock":            "<rpc><lock><target><candidate/></target></lock></rpc>",
+	"rescue-config":   "<rpc><load-configuration rescue=\"rescue\"/></rpc>",
 	"rollback-config": "<rpc><load-configuration rollback=\"%d\"/></rpc>",
-    "unlock":                           "<rpc><unlock><target><candidate/></target></unlock></rpc>",    
+	"unlock":          "<rpc><unlock><target><candidate/></target></unlock></rpc>",
 }
