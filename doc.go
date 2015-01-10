0efb76e7eada37113190a7e561713291834f0d5e
Scott Ware <scottdware@gmail.com>
2015-01-10 10:49:55 -0500

    Updated documentation

--- doc.go
+++ doc.go
@@ -1,42 +1,42 @@
 /*
 Package junos allows you to run commands on and configure Junos devices.
 
-	// Establish our session
+Establish our session
 	jnpr := junos.NewSession(host, user, password)
 
-	// Lock config
+Lock config
 	err := jnpr.Lock()
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	// Unlock config
+Unlock config
 	err = jnpr.Unlock()
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	// Rollback diff compare
+Rollback diff compare
 	diff, err := jnpr.RollbackDiff(3)
 	if err != nil {
 		fmt.Printf("Error: %s", err)
 	}
 	fmt.Println(diff)
 
-	// Rollback config
+Rollback config
 	err := jnpr.RollbackConfig(2)
 	if err != nil {
 		fmt.Printf("Error: %s", err)
 	}s
 
-	// Show command
+Show command
 	output, err := jnpr.Command("show version", "text")
 	if err != nil {
 		fmt.Printf("Error: %s", err)
 	}
 	fmt.Println(output)
 
-	// Close the connection
+Close the connection
 	jnpr.Close()
 */
 package junos
