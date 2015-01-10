57bd3a413e4fade0124b7c473981bbf96b4dd110
Scott Ware <scottdware@gmail.com>
2015-01-10 12:12:26 -0500

    Added additional RPC commands for loading configuration

--- rpc.go
+++ rpc.go
@@ -5,6 +5,7 @@ var rpcCommand = map[string]string{
 	"command":                          "<rpc><command format=\"text\">%s</command></rpc>",
 	"command-xml":                      "<rpc><command format=\"xml\">%s</command></rpc>",
 	"commit":                           "<rpc><commit-configuration/></rpc>",
+	"configure-set":                    "<rpc><load-configuration action=\"set\" format=\"text\"><configuration-set>%s</configuration-set></load-configuration></rpc>",
 	"get-rescue-information":           "<rpc><get-rescue-information><format>text</format></get-rescue-information></rpc>",
 	"get-rollback-information":         "<rpc><get-rollback-information><rollback>%d</rollback><format>text</format></get-rollback-information></rpc>",
 	"get-rollback-information-compare": "<rpc><get-rollback-information><rollback>0</rollback><compare>%d</compare><format>text</format></get-rollback-information></rpc>",
