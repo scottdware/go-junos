838865eab86ad082983a49932a61be69597a6e9d
Scott Ware <scottdware@gmail.com>
2015-01-10 09:27:26 -0500

    Added additional RPC commands

--- rpc.go
+++ rpc.go
@@ -2,11 +2,13 @@ package junos
 
 // rpcCommand lists the commands that will be called.
 var rpcCommand = map[string]string{
-	"lock":                             "<rpc><lock><target><candidate/></target></lock></rpc>",
-	"unlock":                           "<rpc><unlock><target><candidate/></target></unlock></rpc>",
 	"command":                          "<rpc><command format=\"text\">%s</command></rpc>",
 	"command-xml":                      "<rpc><command format=\"xml\">%s</command></rpc>",
+    "commit": "<rpc><commit-configuration/></rpc>",
 	"get-rescue-information":           "<rpc><get-rescue-information><format>text</format></get-rescue-information></rpc>",
 	"get-rollback-information":         "<rpc><get-rollback-information><rollback>%d</rollback><format>text</format></get-rollback-information></rpc>",
 	"get-rollback-information-compare": "<rpc><get-rollback-information><rollback>0</rollback><compare>%d</compare><format>text</format></get-rollback-information></rpc>",
+	"lock":                             "<rpc><lock><target><candidate/></target></lock></rpc>",
+	"rollback-config": "<rpc><load-configuration rollback=\"%d\"/></rpc>",
+    "unlock":                           "<rpc><unlock><target><candidate/></target></unlock></rpc>",    
 }
