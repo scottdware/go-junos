34087f2601ed98c030ddbf1af94b6a2974a68b92
Scott Ware <scottdware@gmail.com>
2015-01-12 14:42:59 -0500

    Added more commit functions, and cleaned up error checking

--- junos.go
+++ junos.go
@@ -15,19 +15,26 @@ type Junos struct {
 	*netconf.Session
 }
 
-// rollbackXML parses our rollback diff configuration.
-type rollbackXML struct {
-	XMLName xml.Name `xml:"rollback-information"`
-	Config  string   `xml:"configuration-information>configuration-output"`
-}
-
 // CommandXML parses our operational command responses.
 type commandXML struct {
 	Config string `xml:",innerxml"`
 }
 
-type software struct {
-	RES []routingEngine `xml:"multi-routing-engine-item"`
+type commitError struct {
+	Path    string `xml:"error-path"`
+	Element string `xml:"error-info>bad-element"`
+	Message string `xml:"error-message"`
+}
+
+type commitResults struct {
+	XMLName xml.Name      `xml:"commit-results"`
+	Errors  []commitError `xml:"rpc-error"`
+}
+
+// rollbackXML parses our rollback diff configuration.
+type rollbackXML struct {
+	XMLName xml.Name `xml:"rollback-information"`
+	Config  string   `xml:"configuration-information>configuration-output"`
 }
 
 type routingEngine struct {
@@ -37,70 +44,171 @@ type routingEngine struct {
 	Version string `xml:"software-information>package-information>comment"`
 }
 
-// NewSession establishes a new connection to a Junos device that we will use
-// to run our commands against.
-func NewSession(host, user, password string) *Junos {
-	s, err := netconf.DialSSH(host, netconf.SSHConfigPassword(user, password))
-	if err != nil {
-		log.Fatal(err)
-	}
+type software struct {
+	RES []routingEngine `xml:"multi-routing-engine-item"`
+}
 
-	return &Junos{
-		s,
-	}
+// Close disconnects our session to the device.
+func (j *Junos) Close() {
+	j.Transport.Close()
 }
 
 // Commit commits the configuration.
 func (j *Junos) Commit() error {
+	errs := &commitResults{}
 	reply, err := j.Exec(rpcCommand["commit"])
 	if err != nil {
 		return err
 	}
 
-	if reply.Ok == false {
+	if reply.Errors != nil {
 		for _, m := range reply.Errors {
 			return errors.New(m.Message)
+		}
+	}
+
+	err = xml.Unmarshal([]byte(reply.Data), &errs)
+	if err != nil {
+		return err
+	}
+
+	if errs.Errors != nil {
+		for _, m := range errs.Errors {
+			message := fmt.Sprintf("[%s]\n    %s\nError: %s", strings.Trim(m.Path, "[\r\n]"), strings.Trim(m.Element, "[\r\n]"), strings.Trim(m.Message, "[\r\n]"))
+			return errors.New(message)
 		}
 	}
 
 	return nil
 }
 
-// Lock locks the candidate configuration.
-func (j *Junos) Lock() error {
-	reply, err := j.Exec(rpcCommand["lock"])
+// CommitAt commits the configuration at the specified <time>.
+func (j *Junos) CommitAt(time string) error {
+	errs := &commitResults{}
+	command := fmt.Sprintf(rpcCommand["commit-at"], time)
+	reply, err := j.Exec(command)
 	if err != nil {
 		return err
 	}
 
-	if reply.Ok == false {
+	if reply.Errors != nil {
 		for _, m := range reply.Errors {
 			return errors.New(m.Message)
+		}
+	}
+
+	err = xml.Unmarshal([]byte(reply.Data), &errs)
+	if err != nil {
+		return err
+	}
+
+	if errs.Errors != nil {
+		for _, m := range errs.Errors {
+			message := fmt.Sprintf("[%s]\n    %s\nError: %s", strings.Trim(m.Path, "[\r\n]"), strings.Trim(m.Element, "[\r\n]"), strings.Trim(m.Message, "[\r\n]"))
+			return errors.New(message)
 		}
 	}
 
 	return nil
 }
 
-// Unlock unlocks the candidate configuration.
-func (j *Junos) Unlock() error {
-	resp, err := j.Exec(rpcCommand["unlock"])
+// CommitCheck checks the configuration for syntax errors.
+func (j *Junos) CommitCheck() error {
+	errs := &commitResults{}
+	reply, err := j.Exec(rpcCommand["commit-check"])
 	if err != nil {
 		return err
 	}
 
-	if resp.Ok == false {
-		for _, m := range resp.Errors {
+	if reply.Errors != nil {
+		for _, m := range reply.Errors {
 			return errors.New(m.Message)
 		}
 	}
 
+	err = xml.Unmarshal([]byte(reply.Data), &errs)
+	if err != nil {
+		return err
+	}
+
+	if errs.Errors != nil {
+		for _, m := range errs.Errors {
+			message := fmt.Sprintf("[%s]\n    %s\nError: %s", strings.Trim(m.Path, "[\r\n]"), strings.Trim(m.Element, "[\r\n]"), strings.Trim(m.Message, "[\r\n]"))
+			return errors.New(message)
+		}
+	}
+
 	return nil
+}
+
+// CommitConfirm rolls back the configuration after <delay> minutes.
+func (j *Junos) CommitConfirm(delay int) error {
+	errs := &commitResults{}
+	command := fmt.Sprintf(rpcCommand["commit-confirm"], delay)
+	reply, err := j.Exec(command)
+	if err != nil {
+		return err
+	}
+
+	if reply.Errors != nil {
+		for _, m := range reply.Errors {
+			return errors.New(m.Message)
+		}
+	}
+
+	err = xml.Unmarshal([]byte(reply.Data), &errs)
+	if err != nil {
+		return err
+	}
+
+	if errs.Errors != nil {
+		for _, m := range errs.Errors {
+			message := fmt.Sprintf("[%s]\n    %s\nError: %s", strings.Trim(m.Path, "[\r\n]"), strings.Trim(m.Element, "[\r\n]"), strings.Trim(m.Message, "[\r\n]"))
+			return errors.New(message)
+		}
+	}
+
+	return nil
+}
+
+// Command runs any operational mode command, such as "show" or "request."
+// Format is either "text" or "xml".
+func (j *Junos) Command(cmd, format string) (string, error) {
+	c := &commandXML{}
+	var command string
+
+	switch format {
+	case "xml":
+		command = fmt.Sprintf(rpcCommand["command-xml"], cmd)
+	default:
+		command = fmt.Sprintf(rpcCommand["command"], cmd)
+	}
+	reply, err := j.Exec(command)
+	if err != nil {
+		return "", err
+	}
+
+	if reply.Errors != nil {
+		for _, m := range reply.Errors {
+			return "", errors.New(m.Message)
+		}
+	}
+
+	err = xml.Unmarshal([]byte(reply.Data), &c)
+	if err != nil {
+		return "", err
+	}
+
+	if c.Config == "" {
+		return "No output available.", nil
+	}
+
+	return c.Config, nil
 }
 
 // LoadConfig loads a given configuration file locally or from
 // an FTP or HTTP server. Format is either "set" "text" or "xml."
-func (j *Junos) LoadConfig(path, format string) error {
+func (j *Junos) LoadConfig(path, format string, commit bool) error {
 	var command string
 	switch format {
 	case "set":
@@ -143,13 +251,49 @@ func (j *Junos) LoadConfig(path, format string) error {
 		return err
 	}
 
-	if reply.Ok == false {
+	if commit {
+		err = j.Commit()
+		if err != nil {
+			return err
+		}
+	}
+
+	if reply.Errors != nil {
 		for _, m := range reply.Errors {
 			return errors.New(m.Message)
 		}
 	}
 
 	return nil
+}
+
+// Lock locks the candidate configuration.
+func (j *Junos) Lock() error {
+	reply, err := j.Exec(rpcCommand["lock"])
+	if err != nil {
+		return err
+	}
+
+	if reply.Errors != nil {
+		for _, m := range reply.Errors {
+			return errors.New(m.Message)
+		}
+	}
+
+	return nil
+}
+
+// NewSession establishes a new connection to a Junos device that we will use
+// to run our commands against.
+func NewSession(host, user, password string) *Junos {
+	s, err := netconf.DialSSH(host, netconf.SSHConfigPassword(user, password))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return &Junos{
+		s,
+	}
 }
 
 // RollbackConfig loads and commits the configuration of a given rollback or rescue state.
@@ -159,9 +303,9 @@ func (j *Junos) RollbackConfig(option interface{}) error {
 	case int:
 		command = fmt.Sprintf(rpcCommand["rollback-config"], option)
 	case string:
-        if option == "rescue" {
-            command = fmt.Sprintf(rpcCommand["rescue-config"])
-        }
+		if option == "rescue" {
+			command = fmt.Sprintf(rpcCommand["rescue-config"])
+		}
 	}
 
 	reply, err := j.Exec(command)
@@ -174,7 +318,7 @@ func (j *Junos) RollbackConfig(option interface{}) error {
 		return err
 	}
 
-	if reply.Ok == false {
+	if reply.Errors != nil {
 		for _, m := range reply.Errors {
 			return errors.New(m.Message)
 		}
@@ -192,7 +336,7 @@ func (j *Junos) RollbackDiff(compare int) (string, error) {
 		return "", err
 	}
 
-	if reply.Ok == false {
+	if reply.Errors != nil {
 		for _, m := range reply.Errors {
 			return "", errors.New(m.Message)
 		}
@@ -206,46 +350,6 @@ func (j *Junos) RollbackDiff(compare int) (string, error) {
 	return rb.Config, nil
 }
 
-// Command runs any operational mode command, such as "show" or "request."
-// Format is either "text" or "xml".
-func (j *Junos) Command(cmd, format string) (string, error) {
-	c := &commandXML{}
-	var command string
-
-	switch format {
-	case "xml":
-		command = fmt.Sprintf(rpcCommand["command-xml"], cmd)
-	default:
-		command = fmt.Sprintf(rpcCommand["command"], cmd)
-	}
-	reply, err := j.Exec(command)
-	if err != nil {
-		return "", err
-	}
-
-	if reply.Ok == false {
-		for _, m := range reply.Errors {
-			return "", errors.New(m.Message)
-		}
-	}
-
-	err = xml.Unmarshal([]byte(reply.Data), &c)
-	if err != nil {
-		return "", err
-	}
-
-	if c.Config == "" {
-		return "No output available.", nil
-	}
-
-	return c.Config, nil
-}
-
-// Close disconnects our session to the device.
-func (j *Junos) Close() {
-	j.Transport.Close()
-}
-
 // Software displays basic information about the device, such as software, hardware, etc.
 func (j *Junos) Software() (*software, error) {
 	data := &software{}
@@ -254,7 +358,7 @@ func (j *Junos) Software() (*software, error) {
 		return nil, err
 	}
 
-	if reply.Ok == false {
+	if reply.Errors != nil {
 		for _, m := range reply.Errors {
 			return nil, errors.New(m.Message)
 		}
@@ -266,4 +370,20 @@ func (j *Junos) Software() (*software, error) {
 	}
 
 	return data, nil
+}
+
+// Unlock unlocks the candidate configuration.
+func (j *Junos) Unlock() error {
+	resp, err := j.Exec(rpcCommand["unlock"])
+	if err != nil {
+		return err
+	}
+
+	if resp.Ok == false {
+		for _, m := range resp.Errors {
+			return errors.New(m.Message)
+		}
+	}
+
+	return nil
 }
