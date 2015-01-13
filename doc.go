8d27890ffc5921c50457d4a74b284cc9333b7399
Scott Ware <scottdware@gmail.com>
2015-01-13 07:10:26 -0500

    Added doc.go documentation file

---
+++ doc.go
@@ -0,0 +1,15 @@
+/*
+package junos
+
+Package junos provides automation for Junos (Juniper Networks) devices.
+
+Establishing A Session
+
+To connect to a Junos device, the process is fairly straightforward.
+
+    jnpr := junos.NewSession(host, user, password)
+    defer jnpr.Close()
+
+
+*/
+package junos
