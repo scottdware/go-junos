0b58885c68f25133a2262271826601ed34f1d36d
Scott Ware <scottdware@gmail.com>
2015-01-13 07:14:17 -0500

    Updated documentation

--- doc.go
+++ doc.go
@@ -10,6 +10,18 @@ To connect to a Junos device, the process is fairly straightforward.
     jnpr := junos.NewSession(host, user, password)
     defer jnpr.Close()
 
+Automation
+
+Once connected, you can run many different commands to interact with the device.
+If you want to view the difference between the current configuration and a rollback
+one, then you can use the RollbackDiff() function.
+
+    diff, err := jnpr.RollbackDiff(3)
+    if err != nil {
+        fmt.Println(err)
+    }
+    fmt.Println(diff)
+    
 
 */
 package junos
