b609ebc8193817bacb5b0f777f9ad52698d47ea6
Scott Ware <scottdware@gmail.com>
2015-01-31 23:04:08 -0500

    Added Security Director functions

---
+++ sd.go
@@ -0,0 +1,569 @@
+package junos
+
+import (
+	"encoding/xml"
+	"errors"
+	"fmt"
+	"net/url"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+// Addresses holds all of our address objects when queried.
+type Addresses struct {
+	Addresses []Address `xml:"address"`
+}
+
+// Address holds information about each individual address object.
+type Address struct {
+	ID          int    `xml:"id"`
+	Name        string `xml:"name"`
+	AddressType string `xml:"address-type"`
+	Description string `xml:"description"`
+	IPAddress   string `xml:"ip-address"`
+}
+
+// Services holds all of our service objects when queried.
+type Services struct {
+	Services []Service `xml:"service"`
+}
+
+// Service holds information about each individual service object.
+type Service struct {
+	ID          int    `xml:"id"`
+	Name        string `xml:"name"`
+	IsGroup     bool   `xml:"is-group"`
+	Description string `xml:"description"`
+}
+
+// Policy holds information about each individual firewall policy.
+type Policy struct {
+	ID          int    `xml:"id"`
+	Name        string `xml:"name"`
+	Description string `xml:"description"`
+}
+
+// Policiese holds all of our firewall policy information.
+type Policies struct {
+	Policies []Policy `xml:"firewall-policy"`
+}
+
+// SecurityDevices holds a []Device slice of every security device within Space.
+type SecurityDevices struct {
+	XMLName xml.Name         `xml:"devices"`
+	Devices []SecurityDevice `xml:"device"`
+}
+
+// SecurityDevice holds all the information about each security device within Space.
+type SecurityDevice struct {
+	ID        int    `xml:"id"`
+	Family    string `xml:"device-family"`
+	Platform  string `xml:"platform"`
+	IPAddress string `xml:"device-ip"`
+	Name      string `xml:"name"`
+}
+
+// addressesXML is what we send to the server when creating an address object.
+var addressesXML = `
+<address>
+    <name>%s</name>
+    <address-type>%s</address-type>
+    <host-name/>
+    <edit-version/>
+    <members/>
+    <address-version>IPV4</address-version>
+    <definition-type>CUSTOM</definition-type>
+    <ip-address>%s</ip-address>
+    <description>%s</description>
+</address>
+`
+
+// serviceXML is what we send to the server when creating a service object.
+var serviceXML = `
+<service>
+    <name>%s</name>
+    <description>%s</description>
+    <is-group>false</is-group>
+    <protocols>
+        <protocol>
+            <name>%s</name>
+            <dst-port>%s</dst-port>
+            <sunrpc-protocol-type>%s</sunrpc-protocol-type>
+            <msrpc-protocol-type>%s</msrpc-protocol-type>
+            <protocol-number>%d</protocol-number>
+            <protocol-type>%s</protocol-type>
+            <disable-timeout>%s</disable-timeout>
+            %s
+        </protocol>
+    </protocols>
+</service>
+`
+
+// addressGroupXML is what we send to the server when adding an address group.
+var addressGroupXML = `
+<address>
+    <name>%s</name>
+    <address-type>GROUP</address-type>
+    <host-name/>
+    <edit-version/>
+    <address-version>IPV4</address-version>
+    <definition-type>CUSTOM</definition-type>
+    <description>%s</description>
+</address>
+`
+
+// serviceGroupXML is what we send to the server when adding a service group.
+var serviceGroupXML = `
+<service>
+    <name>%s</name>
+    <is-group>true</is-group>
+    <description>%s</description>    
+</service>
+`
+
+// removeXML is what we send to the server when removing an address or service from a group.
+var removeXML = `
+<diff>
+    <remove sel="%s/members/member[name='%s']"/>
+</diff>
+`
+
+// addGroupMemberXML is what we send to the server when adding addresses or services to a group.
+var addGroupMemberXML = `
+<diff>
+    <add sel="%s/members">
+        <member>
+            <name>%s</name>
+        </member>
+    </add>
+</diff>
+`
+
+// renameXML is what we send to the server when renaming an address or service object.
+var renameXML = `
+<diff>
+    <replace sel="%s/name">
+        <name>%s</name>
+    </replace>
+</diff>
+`
+
+// updateDeviceXML is what we send to the server when updating a security device.
+var updateDeviceXML = `
+<update-devices>
+    <sd-ids>
+        <id>%d</id>
+    </sd-ids>
+    <service-types>
+        <service-type>POLICY</service-type>
+    </service-types>
+    <update-options>
+        <enable-policy-rematch-srx-only>false</enable-policy-rematch-srx-only>
+    </update-options>
+</update-devices>
+`
+
+// publishPolicyXML is what we send to the server to publish a changed policy.
+var publishPolicyXML = `
+<publish>
+    <policy-ids>
+        <policy-id>%d</policy-id>
+    </policy-ids>
+</publish>
+`
+
+// getObjectID returns the given address object ID, which will be used for REST
+// calls against it.
+func (s *JunosSpace) getObjectID(object interface{}, service bool) (int, error) {
+	var err error
+	var objectID int
+	var services *Services
+	ipRegex := regexp.MustCompile(`(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\/\d+)`)
+	if service {
+		services, err = s.Services(object.(string))
+	}
+	objects, err := s.Addresses(object.(string))
+	if err != nil {
+		return 0, err
+	}
+
+	switch object.(type) {
+	case int:
+		objectID = object.(int)
+	case string:
+		if service {
+			for _, o := range services.Services {
+				if o.Name == object {
+					objectID = o.ID
+				}
+			}
+		}
+		if ipRegex.MatchString(object.(string)) {
+			for _, o := range objects.Addresses {
+				if o.IPAddress == object {
+					objectID = o.ID
+				}
+			}
+		}
+		for _, o := range objects.Addresses {
+			if o.Name == object {
+				objectID = o.ID
+			}
+		}
+	}
+
+	return objectID, nil
+}
+
+// getPolicyID returns the given firewall policy object ID, which will be used for REST
+// calls against it.
+func (s *JunosSpace) getPolicyID(object string) (int, error) {
+	var err error
+	var objectID int
+	objects, err := s.Policies()
+	if err != nil {
+		return 0, err
+	}
+
+	for _, o := range objects.Policies {
+		if o.Name == object {
+			objectID = o.ID
+		}
+	}
+
+	return objectID, nil
+}
+
+// Addresses queries the Junos Space server and returns all of the information
+// about each address that is managed by Space.
+func (s *JunosSpace) Addresses(filter string) (*Addresses, error) {
+	var addresses Addresses
+	p := url.Values{}
+	p.Set("filter", fmt.Sprintf("(global eq '%s')", filter))
+	req := &APIRequest{
+		Method: "get",
+		URL:    fmt.Sprintf("/api/juniper/sd/address-management/addresses?%s", p.Encode()),
+	}
+	data, err := s.APICall(req)
+	if err != nil {
+		return nil, err
+	}
+
+	err = xml.Unmarshal(data, &addresses)
+	if err != nil {
+		return nil, err
+	}
+
+	return &addresses, nil
+}
+
+// AddAddress adds a new address object to Junos Space, and returns the Job ID.
+func (s *JunosSpace) AddAddress(name, ip, desc string) (int, error) {
+	var job jobID
+	var addrType = "NETWORK"
+
+	if strings.Contains(ip, "/32") {
+		addrType = "IPADDRESS"
+	}
+
+	address := fmt.Sprintf(addressesXML, name, addrType, ip, desc)
+	req := &APIRequest{
+		Method:      "post",
+		URL:         "/api/juniper/sd/address-management/addresses",
+		Body:        address,
+		ContentType: ContentAddress,
+	}
+	data, err := s.APICall(req)
+	if err != nil {
+		return 0, err
+	}
+
+	err = xml.Unmarshal(data, &job)
+	if err != nil {
+		return 0, err
+	}
+
+	return job.ID, nil
+}
+
+// AddService adds a new service object to Junos Space, and returns the Job ID.
+func (s *JunosSpace) AddService(proto, name string, low, high int, desc string, timeout int) (int, error) {
+	var job jobID
+	var port string
+	var protoNumber int
+	var inactivity string
+	var secs string
+	ptype := fmt.Sprintf("PROTOCOL_%s", strings.ToUpper(proto))
+	protocol := strings.ToUpper(proto)
+
+	protoNumber = 6
+	if proto == "udp" {
+		protoNumber = 17
+	}
+
+	port = strconv.Itoa(low)
+	if low < high {
+		port = fmt.Sprintf("%s-%s", strconv.Itoa(low), strconv.Itoa(high))
+	}
+
+	inactivity = "false"
+	secs = fmt.Sprintf("<inactivity-timeout>%d</inactivity-timeout>", timeout)
+	if timeout == 0 {
+		inactivity = "true"
+		secs = "<inactivity-timeout/>"
+	}
+
+	service := fmt.Sprintf(serviceXML, name, desc, name, port, protocol, protocol, protoNumber, ptype, inactivity, secs)
+	req := &APIRequest{
+		Method:      "post",
+		URL:         "/api/juniper/sd/service-management/services",
+		Body:        service,
+		ContentType: ContentService,
+	}
+	data, err := s.APICall(req)
+	if err != nil {
+		return 0, err
+	}
+
+	err = xml.Unmarshal(data, &job)
+	if err != nil {
+		return 0, err
+	}
+
+	return job.ID, nil
+}
+
+// AddGroup adds a new address or service group to Junos Space, and returns the Job ID.
+func (s *JunosSpace) AddGroup(isservice bool, name, desc string) error {
+	uri := "/api/juniper/sd/address-management/addresses"
+	addGroupXML := addressGroupXML
+	content := ContentAddress
+
+	if isservice {
+		uri = "/api/juniper/sd/service-management/services"
+		addGroupXML = serviceGroupXML
+		content = ContentService
+	}
+
+	groupXML := fmt.Sprintf(addGroupXML, name, desc)
+	req := &APIRequest{
+		Method:      "post",
+		URL:         uri,
+		Body:        groupXML,
+		ContentType: content,
+	}
+	_, err := s.APICall(req)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// ModifyObject modifies an existing address, service or group. The actions are as follows:
+//
+// "isservice" is either "true" (for service) or "false" (for address)
+//
+// ModifyObject("isservice", "add", "existing-group", "member-to-add")
+// ModifyObject("isservice", "remove", "existing-group", "member-to-remove")
+// ModifyObject("isservice", "rename", "old-name", "new-name")
+// ModifyObject("isservice", "delete", "object-to-delete")
+func (s *JunosSpace) ModifyObject(isservice bool, actions ...interface{}) error {
+	var err error
+	var uri string
+	var content string
+	var rel string
+	objectID, err := s.getObjectID(actions[1], isservice)
+	if err != nil {
+		return err
+	}
+
+	if objectID != 0 {
+		var req *APIRequest
+		uri = fmt.Sprintf("/api/juniper/sd/address-management/addresses/%d", objectID)
+		content = ContentAddressPatch
+		rel = "address"
+
+		if isservice {
+			uri = fmt.Sprintf("/api/juniper/sd/service-management/services/%d", objectID)
+			content = ContentServicePatch
+			rel = "service"
+		}
+
+		switch actions[0] {
+		case "add":
+			req = &APIRequest{
+				Method:      "patch",
+				URL:         uri,
+				Body:        fmt.Sprintf(addGroupMemberXML, rel, actions[2]),
+				ContentType: content,
+			}
+		case "remove":
+			req = &APIRequest{
+				Method:      "patch",
+				URL:         uri,
+				Body:        fmt.Sprintf(removeXML, rel, actions[2]),
+				ContentType: content,
+			}
+		case "rename":
+			req = &APIRequest{
+				Method:      "patch",
+				URL:         uri,
+				Body:        fmt.Sprintf(renameXML, rel, actions[2]),
+				ContentType: content,
+			}
+		case "delete":
+			req = &APIRequest{
+				Method: "delete",
+				URL:    uri,
+			}
+		}
+
+		_, err = s.APICall(req)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+// Services queries the Junos Space server and returns all of the information
+// about each service that is managed by Space.
+func (s *JunosSpace) Services(filter string) (*Services, error) {
+	var services Services
+	p := url.Values{}
+	p.Set("filter", fmt.Sprintf("(global eq '%s')", filter))
+	req := &APIRequest{
+		Method: "get",
+		URL:    fmt.Sprintf("/api/juniper/sd/service-management/services?%s", p.Encode()),
+	}
+	data, err := s.APICall(req)
+	if err != nil {
+		return nil, err
+	}
+
+	err = xml.Unmarshal(data, &services)
+	if err != nil {
+		return nil, err
+	}
+
+	return &services, nil
+}
+
+// SecurityDevices queries the Junos Space server and returns all of the information
+// about each security device that is managed by Space.
+func (s *JunosSpace) SecurityDevices() (*SecurityDevices, error) {
+	var devices SecurityDevices
+	req := &APIRequest{
+		Method: "get",
+		URL:    "/api/juniper/sd/device-management/devices",
+	}
+	data, err := s.APICall(req)
+	if err != nil {
+		return nil, err
+	}
+
+	err = xml.Unmarshal(data, &devices)
+	if err != nil {
+		return nil, err
+	}
+
+	return &devices, nil
+}
+
+// Policies returns a list of all firewall policies managed by Junos Space.
+func (s *JunosSpace) Policies() (*Policies, error) {
+	var policies Policies
+	req := &APIRequest{
+		Method: "get",
+		URL:    "/api/juniper/sd/fwpolicy-management/firewall-policies",
+	}
+	data, err := s.APICall(req)
+	if err != nil {
+		return nil, err
+	}
+
+	err = xml.Unmarshal(data, &policies)
+	if err != nil {
+		return nil, err
+	}
+
+	return &policies, nil
+}
+
+// PublishPolicy publishes a changed firewall policy. If "true" is specified for
+// <update>, then Junos Space will also update the device.
+func (s *JunosSpace) PublishPolicy(object interface{}, update bool) (int, error) {
+	var err error
+	var job jobID
+	var id int
+	var uri = "/api/juniper/sd/fwpolicy-management/publish"
+
+	switch object.(type) {
+	case int:
+		id = object.(int)
+	case string:
+		id, err = s.getPolicyID(object.(string))
+		if err != nil {
+			return 0, err
+		}
+		if id == 0 {
+			return 0, errors.New("No policy found")
+		}
+	}
+	publish := fmt.Sprintf(publishPolicyXML, id)
+
+	if update {
+		uri = "/api/juniper/sd/fwpolicy-management/publish?update=true"
+	}
+
+	req := &APIRequest{
+		Method:      "post",
+		URL:         uri,
+		Body:        publish,
+		ContentType: ContentPublish,
+	}
+	data, err := s.APICall(req)
+	if err != nil {
+		return 0, err
+	}
+
+	err = xml.Unmarshal(data, &job)
+	if err != nil {
+		return 0, errors.New("No policy changes to publish")
+	}
+
+	return job.ID, nil
+}
+
+// UpdateDevice will update a changed security device, synchronizing it with
+// Junos Space.
+func (s *JunosSpace) UpdateDevice(device interface{}) (int, error) {
+	var job jobID
+	deviceID, err := s.getDeviceID(device, true)
+	if err != nil {
+		return 0, err
+	}
+
+	update := fmt.Sprintf(updateDeviceXML, deviceID)
+	req := &APIRequest{
+		Method:      "post",
+		URL:         "/api/juniper/sd/device-management/update-devices",
+		Body:        update,
+		ContentType: ContentUpdateDevices,
+	}
+	data, err := s.APICall(req)
+	if err != nil {
+		return 0, err
+	}
+
+	err = xml.Unmarshal(data, &job)
+	if err != nil {
+		return 0, err
+	}
+
+	return job.ID, nil
+}
