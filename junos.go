6e38fbd799ef2478b090c7d999e1168bd644b858
Scott Ware <scottdware@gmail.com>
2015-02-04 14:39:03 -0500

    Added all our RPC variables

--- junos.go
+++ junos.go
@@ -11,6 +11,37 @@ import (
 	"strings"
 )
 
+// All of our RPC calls we use.
+var (
+	rpcCommand             = "<rpc><command format=\"text\">%s</command></rpc>"
+	rpcCommandXML          = "<rpc><command format=\"xml\">%s</command></rpc>"
+	rpcCommit              = "<rpc><commit-configuration/></rpc>"
+	rpcCommitAt            = "<rpc><commit-configuration><at-time>%s</at-time></commit-configuration></rpc>"
+	rpcCommitCheck         = "<rpc><commit-configuration><check/></commit-configuration></rpc>"
+	rpcCommitConfirm       = "<rpc><commit-configuration><confirmed/><confirm-timeout>%d</confirm-timeout></commit-configuration></rpc>"
+	rpcFactsRE             = "<rpc><get-route-engine-information/></rpc>"
+	rpcFactsChassis        = "<rpc><get-chassis-inventory/></rpc>"
+	rpcConfigFileSet       = "<rpc><load-configuration action=\"set\" format=\"text\"><configuration-set>%s</configuration-set></load-configuration></rpc>"
+	rpcConfigFileText      = "<rpc><load-configuration format=\"text\"><configuration-text>%s</configuration-text></load-configuration></rpc>"
+	rpcConfigFileXML       = "<rpc><load-configuration format=\"xml\"><configuration>%s</configuration></load-configuration></rpc>"
+	rpcConfigURLSet        = "<rpc><load-configuration action=\"set\" format=\"text\" url=\"%s\"/></rpc>"
+	rpcConfigURLText       = "<rpc><load-configuration format=\"text\" url=\"%s\"/></rpc>"
+	rpcConfigURLXML        = "<rpc><load-configuration format=\"xml\" url=\"%s\"/></rpc>"
+	rpcGetRescue           = "<rpc><get-rescue-information><format>text</format></get-rescue-information></rpc>"
+	rpcGetRollback         = "<rpc><get-rollback-information><rollback>%d</rollback><format>text</format></get-rollback-information></rpc>"
+	rpcGetRollbackCompare  = "<rpc><get-rollback-information><rollback>0</rollback><compare>%d</compare><format>text</format></get-rollback-information></rpc>"
+	rpcHardware            = "<rpc><get-chassis-inventory/></rpc>"
+	rpcLock                = "<rpc><lock><target><candidate/></target></lock></rpc>"
+	rpcRescueConfig        = "<rpc><load-configuration rescue=\"rescue\"/></rpc>"
+	rpcRescueDelete        = "<rpc><request-delete-rescue-configuration/></rpc>"
+	rpcRescueSave          = "<rpc><request-save-rescue-configuration/></rpc>"
+	rpcRollbackConfig      = "<rpc><load-configuration rollback=\"%d\"/></rpc>"
+	rpcRoute               = "<rpc><get-route-engine-information/></rpc>"
+	rpcSoftware            = "<rpc><get-software-information/></rpc>"
+	rpcUnlock              = "<rpc><unlock><target><candidate/></target></unlock></rpc>"
+	rpcVersion             = "<rpc><get-software-information/></rpc>"
+)
+
 // Junos contains our session state.
 type Junos struct {
 	Session      *netconf.Session
