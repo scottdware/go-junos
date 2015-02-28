bdef24da6f0938657585318b4ff11e5062f82992
Scott Ware <scottdware@gmail.com>
2015-02-28 11:34:49 -0500

    Testing data validation

--- sd.go
+++ sd.go
@@ -84,7 +84,7 @@ type existingVariable struct {
 	Version            int              `xml:"edit-version"`
 	DefaultName        string           `xml:"default-name"`
 	DefaultValue       string              `xml:"default-value-detail>default-value"`
-	VariableValuesList []variableValues `xml:"variable-values-list"`
+	VariableValuesList []variableValues `xml:"variable-values-list>variable-values"`
 }
 
 type variableValues struct {
