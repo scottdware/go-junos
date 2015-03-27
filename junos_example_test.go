00a40b56720aef1dc2e20c150f7fb107ef0982cd
Scott Ware <scottdware@gmail.com>
2015-03-27 07:54:13 -0400

    Added more examples

--- junos_example_test.go
+++ junos_example_test.go
@@ -8,3 +8,16 @@ func Example() {
 	}
 	defer jnpr.Close()
 }
+
+// To View the entire configuration, use the keyword "full" for the first
+// argument. If anything else outside of "full" is specified, it will return
+// the configuration of the specified top-level stanza only. So "security" would return everything
+// under the "security" stanza.
+func Example_viewConfiguration() {
+	// Output format can be "text" or "xml"
+	config, err := jnpr.GetConfig("full", "text")
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(config)
+}
