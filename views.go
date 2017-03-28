dfee297159f0b16619a75d7980dff4e17fe690eb
Scott Ware <scottdware@gmail.com>
2017-03-28 10:40:23 -0400

    Added more views

--- views.go
+++ views.go
@@ -18,7 +18,6 @@ type ArpTable struct {
 type ArpEntry struct {
 	MACAddress string `xml:"mac-address"`
 	IPAddress  string `xml:"ip-address"`
-	Hostname   string `xml:"hostname"`
 	Interface  string `xml:"interface-name"`
 }
 
@@ -112,28 +111,161 @@ type Vlan struct {
 	MemberInterfaces []string `xml:"l2ng-l2rtb-vlan-member>l2ng-l2rtb-vlan-member-interface"`
 }
 
-// Views contains the information for the specific views.
+// EthernetSwitchingTable contains the ethernet-switching table on the device.
+type EthernetSwitchingTable struct {
+	Entries []L2MACEntry `xml:"l2ng-l2ald-mac-entry-vlan"`
+}
+
+// L2MACEntry contains information about every MAC address on each VLAN.
+type L2MACEntry struct {
+	GlobalMACCount  int        `xml:"mac-count-global"`
+	LearnedMACCount int        `xml:"learnt-mac-count"`
+	RoutingInstance string     `xml:"l2ng-l2-mac-routing-instance"`
+	VlanID          int        `xml:"l2ng-l2-vlan-id"`
+	MACEntries      []MACEntry `xml:"l2ng-mac-entry"`
+}
+
+// MACEntry contains information about each individual MAC address. Flags are: S - static MAC, D - dynamic MAC,
+// L - locally learned, P - persistent static, SE - statistics enabled, NM - non configured MAC, R - remote PE MAC,
+// O - ovsdb MAC.
+type MACEntry struct {
+	VlanName         string `xml:"l2ng-l2-mac-vlan-name"`
+	MACAddress       string `xml:"l2ng-l2-mac-address"`
+	Age              string `xml:"l2ng-l2-mac-age"`
+	Flags            string `xml:"l2ng-l2-mac-flags"`
+	LogicalInterface string `xml:"l2ng-l2-mac-logical-interface"`
+}
+
+// HardwareInventory contains all the hardware information about the device.
+type HardwareInventory struct {
+	Chassis []Chassis `xml:"chassis"`
+}
+
+type srxHardwareInventory struct {
+	Chassis []Chassis `xml:"multi-routing-engine-item>chassis-inventory>chassis"`
+}
+
+// Chassis contains all of the hardware information for each chassis, such as a clustered pair of SRX's or a
+// virtual-chassis configuration.
+type Chassis struct {
+	Name         string   `xml:"name"`
+	SerialNumber string   `xml:"serial-number"`
+	Description  string   `xml:"description"`
+	Modules      []Module `xml:"chassis-module"`
+}
+
+// Module contains information about each individual module.
+type Module struct {
+	Name         string      `xml:"name"`
+	Version      string      `xml:"version,omitempty"`
+	PartNumber   string      `xml:"part-number"`
+	SerialNumber string      `xml:"serial-number"`
+	Description  string      `xml:"description"`
+	CLEICode     string      `xml:"clei-code"`
+	ModuleNumber string      `xml:"module-number"`
+	SubModules   []SubModule `xml:"chassis-sub-module"`
+}
+
+// SubModule contains information about each individual sub-module.
+type SubModule struct {
+	Name          string         `xml:"name"`
+	Version       string         `xml:"version,omitempty"`
+	PartNumber    string         `xml:"part-number"`
+	SerialNumber  string         `xml:"serial-number"`
+	Description   string         `xml:"description"`
+	CLEICode      string         `xml:"clei-code"`
+	ModuleNumber  string         `xml:"module-number"`
+	SubSubModules []SubSubModule `xml:"chassis-sub-sub-module"`
+}
+
+// SubSubModule contains information about each sub-sub module, such as SFP's.
+type SubSubModule struct {
+	Name         string `xml:"name"`
+	Version      string `xml:"version,omitempty"`
+	PartNumber   string `xml:"part-number"`
+	SerialNumber string `xml:"serial-number"`
+	Description  string `xml:"description"`
+}
+
+// VirtualChassis contains information regarding the virtual-chassis setup for the device.
+type VirtualChassis struct {
+	PreProvisionedVCID   string     `xml:"preprovisioned-virtual-chassis-information>virtual-chassis-id"`
+	PreProvisionedVCMode string     `xml:"preprovisioned-virtual-chassis-information>virtual-chassis-mode"`
+	Members              []VCMember `xml:"member-list>member"`
+}
+
+// VCMember contains information about each individual virtual-chassis member.
+type VCMember struct {
+	Status       string             `xml:"member-status"`
+	ID           int                `xml:"member-id"`
+	FPCSlot      string             `xml:"fpc-slot"`
+	SerialNumber string             `xml:"member-serial-number"`
+	Model        string             `xml:"member-model"`
+	Priority     int                `xml:"member-priority"`
+	MixedMode    string             `xml:"member-mixed-mode"`
+	RouteMode    string             `xml:"member-route-mode"`
+	Role         string             `xml:"member-role"`
+	Neighbors    []VCMemberNeighbor `xml:"neighbor-list>neighbor"`
+}
+
+// VCMemberNeighbor contains information about each virtual-chassis member neighbor.
+type VCMemberNeighbor struct {
+	ID        int    `xml:"neighbor-id"`
+	Interface string `xml:"neighbor-interface"`
+}
+
+// Views contains the information for the specific views. Note that some views aren't available for specific
+// hardware platforms, such as the "VirtualChassis" view on an SRX.
 type Views struct {
-	Arp       ArpTable
-	Route     RoutingTable
-	Interface Interfaces
-	Vlan      Vlans
+	Arp            ArpTable
+	Route          RoutingTable
+	Interface      Interfaces
+	Vlan           Vlans
+	EthernetSwitch EthernetSwitchingTable
+	Inventory      HardwareInventory
+	VirtualChassis VirtualChassis
 }
 
 var (
 	viewCategories = map[string]string{
-		"arp":       "<get-arp-table-information><no-resolve/></get-arp-table-information>",
-		"route":     "<get-route-information/>",
-		"interface": "<get-interface-information/>",
-		"vlan":      "<get-vlan-information/>",
+		"arp":            "<get-arp-table-information><no-resolve/></get-arp-table-information>",
+		"route":          "<get-route-information/>",
+		"interface":      "<get-interface-information/>",
+		"vlan":           "<get-vlan-information/>",
+		"ethernetswitch": "<get-ethernet-switching-table-information/>",
+		"inventory":      "<get-chassis-inventory/>",
+		"virtualchassis": "<get-virtual-chassis-information/>",
 	}
 )
 
+func validatePlatform(j *Junos, v string) error {
+	switch v {
+	case "ethernetswitch":
+		if strings.Contains(j.Platform[0].Model, "SRX") || strings.Contains(j.Platform[0].Model, "MX") {
+			return errors.New("ethernet-switching information is not available on this platform")
+		}
+	case "virtualchassis":
+		if strings.Contains(j.Platform[0].Model, "SRX") || strings.Contains(j.Platform[0].Model, "MX") {
+			return errors.New("virtual-chassis information is not available on this platform")
+		}
+	}
+
+	return nil
+}
+
 // Views gathers information on the device given the "view" specified. These views can be interrated/looped over to view the
-// information (i.e. ARP table entries, interface details/statistics, routing tables, etc.). Supported views are:
-// arp, route, interface, vlan.
+// data (i.e. ARP table entries, interface details/statistics, routing tables, etc.). Supported views are:
+// arp, route, interface, vlan, ethernetswitch, inventory.
 func (j *Junos) Views(view string) (*Views, error) {
 	var results Views
+
+	if strings.Contains(j.Platform[0].Model, "SRX") || strings.Contains(j.Platform[0].Model, "MX") {
+		err := validatePlatform(j, view)
+
+		if err != nil {
+			return nil, err
+		}
+	}
 
 	reply, err := j.Session.Exec(netconf.RawMethod(viewCategories[view]))
 	if err != nil {
@@ -187,6 +319,47 @@ func (j *Junos) Views(view string) (*Views, error) {
 		}
 
 		results.Vlan = vlan
+	case "ethernetswitch":
+		var ethtable EthernetSwitchingTable
+		formatted := strings.Replace(reply.Data, "\n", "", -1)
+
+		if err := xml.Unmarshal([]byte(formatted), &ethtable); err != nil {
+			return nil, err
+		}
+
+		results.EthernetSwitch = ethtable
+	case "inventory":
+		var inventory HardwareInventory
+		formatted := strings.Replace(reply.Data, "\n", "", -1)
+
+		if strings.Contains(reply.Data, "multi-routing-engine-results") {
+			var srxinventory srxHardwareInventory
+
+			if err := xml.Unmarshal([]byte(formatted), &srxinventory); err != nil {
+				return nil, err
+			}
+
+			for _, c := range srxinventory.Chassis {
+				inventory.Chassis = append(inventory.Chassis, c)
+			}
+
+			results.Inventory = inventory
+		} else {
+			if err := xml.Unmarshal([]byte(formatted), &inventory); err != nil {
+				return nil, err
+			}
+
+			results.Inventory = inventory
+		}
+	case "virtualchassis":
+		var vc VirtualChassis
+		formatted := strings.Replace(reply.Data, "\n", "", -1)
+
+		if err := xml.Unmarshal([]byte(formatted), &vc); err != nil {
+			return nil, err
+		}
+
+		results.VirtualChassis = vc
 	}
 
 	return &results, nil
