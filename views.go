9d49324c2b09034ecd471c46ea7ff91bd039f574
buck6058 <buck.wallander@rackspace.com>
2017-09-28 14:24:53 -0500

    added clustered srx support for staticnat view

--- views.go
+++ views.go
@@ -256,6 +256,12 @@ type StaticNats struct {
 	Entries []StaticNatEntry `xml:"static-nat-rule-entry"`
 }
 
+// srxStaticNats contains static NATs configured across a clustered-mode SRX
+type srxStaticNats struct {
+	Count   int              `xml:"multi-routing-engine-item>static-nat-rule-information>total-static-nat-rules>total-rules"`
+	Entries []StaticNatEntry `xml:"multi-routing-engine-item>static-nat-rule-information"`
+}
+
 // StaticNatEntry holds each individual static NAT entry.
 type StaticNatEntry struct {
 	Name       string `xml:"rule-name"`
@@ -313,7 +319,7 @@ func validatePlatform(j *Junos, v string) error {
 
 // Views gathers information on the device given the "view" specified. These views can be interrated/looped over to view the
 // data (i.e. ARP table entries, interface details/statistics, routing tables, etc.). Supported views are:
-// arp, route, interface, vlan, ethernetswitch, inventory.
+// arp, route, interface, vlan, ethernetswitch, inventory, staticnat.
 func (j *Junos) Views(view string) (*Views, error) {
 	var results Views
 
@@ -431,11 +437,25 @@ func (j *Junos) Views(view string) (*Views, error) {
 		var staticnats StaticNats
 		formatted := strings.Replace(reply.Data, "\n", "", -1)
 
-		if err := xml.Unmarshal([]byte(formatted), &staticnats); err != nil {
-			return nil, err
-		}
+		if strings.Contains(reply.Data, "multi-routing-engine-results") {
+			var srxstaticnats srxStaticNats
 
-		results.StaticNat = staticnats
+			if err := xml.Unmarshal([]byte(formatted), &srxstaticnats); err != nil {
+				return nil, err
+			}
+
+			for _, c := range srxstaticnats.Entries {
+				staticnats.Entries = append(staticnats.Entries, c)
+			}
+
+			results.StaticNat = staticnats
+		} else {
+			if err := xml.Unmarshal([]byte(formatted), &staticnats); err != nil {
+				return nil, err
+			}
+
+			results.StaticNat = staticnats
+		}
 	}
 
 	return &results, nil
